2f50efa2b3f600465c774c1168fea799b5009e60
Timo Newton <tnewton@productiveedge.com>
2017-06-09 11:32:58 -0500

    readjusting swagger generation approach to leverage gradle

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by timonewton on 6/9/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main (String[]args ){
+
+    }
+}
